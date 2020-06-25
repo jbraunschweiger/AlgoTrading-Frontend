bbcb0b040696656fa7388031017b1c463179e912
Jakob Braunschweiger <jbraunschweiger@wustl.edu>
2020-06-25 13:24:12 -0500

    Updates

---
+++ precache-manifest.72472c1196fd00ede01ad857d5038e5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2125b2d4596b8484988638395e7bc513",
+    "url": "/AlgoTrading-Frontend/index.html"
+  },
+  {
+    "revision": "c32d325cab3211b09b01",
+    "url": "/AlgoTrading-Frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "942dcea918823d6473f7",
+    "url": "/AlgoTrading-Frontend/static/js/2.0e1542fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AlgoTrading-Frontend/static/js/2.0e1542fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c32d325cab3211b09b01",
+    "url": "/AlgoTrading-Frontend/static/js/main.9e75bead.chunk.js"
+  },
+  {
+    "revision": "cf7d64e1f12035bc4b2c",
+    "url": "/AlgoTrading-Frontend/static/js/runtime-main.53520e89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AlgoTrading-Frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
